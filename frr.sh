39f01572750041094d279e8f29ea23b8c51d211b
metastable-void <git@yuka.h09.eu>
2023-11-23 00:51:35 +0900

    remove unnecessary validation

--- frr.sh
+++ frr.sh
@@ -24,13 +24,6 @@ vtysh -c 'show ip bgp ipv4 unicast json' > $BGP_IPV4_JSON_OUTPUT || {
   exit 1
 }
 
-# Check if the JSON output is valid
-jq empty $BGP_IPV4_JSON_OUTPUT > /dev/null 2>&1 || {
-  rm -f $BGP_IPV4_JSON_OUTPUT
-  echo "Error: Invalid BGP IPv4 JSON output"
-  exit 1
-}
-
 mv $BGP_IPV4_JSON_OUTPUT $BGP_IPV4_JSON
 
 datetime=$(date +%Y%m%d%H%M%S)
@@ -38,13 +31,6 @@ datetime=$(date +%Y%m%d%H%M%S)
 vtysh -c 'show ip bgp ipv6 unicast json' > $BGP_IPV6_JSON_OUTPUT || {
   rm -f $BGP_IPV6_JSON_OUTPUT
   echo "Error: Unable to get BGP IPv6 JSON output"
-  exit 1
-}
-
-# Check if the JSON output is valid
-jq empty $BGP_IPV6_JSON_OUTPUT > /dev/null 2>&1 || {
-  rm -f $BGP_IPV6_JSON_OUTPUT
-  echo "Error: Invalid BGP IPv6 JSON output"
   exit 1
 }
 
